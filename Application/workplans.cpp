c283dd0b10df7f6e5cc777df4f7785845f6f6b00
Viacheslav <ggg9gg6@yandex.ru>
2021-08-31 22:48:33 +0300

    Creating a Handler for the result of clicking the saveButton (workplans.cpp)

--- Application/workplans.cpp
+++ Application/workplans.cpp
@@ -49,3 +49,87 @@ workplans::~workplans()
 {
     delete ui;
 }
+
+
+void workplans::on_saveButton_clicked()
+{
+    bool checker = true;
+
+    if (ui->direction_LineEdit->text() == "")
+    {
+        checker = false;
+        QMessageBox::about(this,"Error!", "No direction !");
+    }
+    if (ui->tasks_LineEdit->text() == "")
+    {
+        checker = false;
+        QMessageBox::about(this,"Error!", "No tasks !");
+    }
+    if (ui->result_LineEdit->text() == "")
+    {
+        checker = false;
+        QMessageBox::about(this,"Error!", "No result!");
+    }
+    if (ui->deadlines_LineEdit->text() == "")
+    {
+        checker = false;
+        QMessageBox::about(this,"Error!", "No deadlines !");
+    }
+    if (ui->main_LineEdit->text() == "")
+    {
+        checker = false;
+        QMessageBox::about(this,"Error!", "No main !");
+    }
+
+    QSqlDatabase myApplicationDB = QSqlDatabase::database("database");
+
+    if (checker == true)
+    {
+        QSqlQuery myQuery = QSqlQuery(myApplicationDB);
+        if (!Editing)
+        {
+            myQuery.prepare("SELECT * FROM function_add_workplan(:_direction , :_tasks, :_result ,:_deadlines ,:_main);");
+            myQuery.bindValue(":_direction", ui->direction_LineEdit->text());
+            myQuery.bindValue(":_tasks", ui->tasks_LineEdit->text());
+            myQuery.bindValue(":_result", ui->result_LineEdit->text());
+            myQuery.bindValue(":_deadlines", ui->deadlines_LineEdit->text());
+            myQuery.bindValue(":_main", ui->main_LineEdit->text());
+            checker = myQuery.exec();
+            if (checker)
+            {
+                QMessageBox::about(this,"Welcome!", "Новый план методической работы!");
+                ui->direction_LineEdit->setText("");
+                ui->tasks_LineEdit->setText("");
+                ui->result_LineEdit->setText("");
+                ui->deadlines_LineEdit->setText("");
+                ui->main_LineEdit->setText("");
+            }
+            if (!checker)
+                QMessageBox::about(this,"Error!", "Что-то пошло не так!");
+        }
+        else
+        {
+            myQuery.prepare("SELECT * FROM function_change_workplan(:_prevdirection ,:_newdirection , :_newtasks , :_newresult ,:_newdeadlines ,:_newmain);");
+            myQuery.bindValue(":_prevdirection", _prevdirection);
+            myQuery.bindValue(":_newdirection", ui->direction_LineEdit->text());
+            myQuery.bindValue(":_newtasks", ui->tasks_LineEdit->text());
+            myQuery.bindValue(":_newresult", ui->result_LineEdit->text());
+            myQuery.bindValue(":_newdeadlines", ui->deadlines_LineEdit->text());
+            myQuery.bindValue(":_newmain", ui->main_LineEdit->text());
+            checker = myQuery.exec();
+            if (checker)
+            {
+                trayIcon = new QSystemTrayIcon(this);
+                trayIcon->setVisible(true);
+                trayIcon->showMessage("Well done!", "планы работы были обновлены");
+                ui->direction_LineEdit->setText("");
+                ui->tasks_LineEdit->setText("");
+                ui->result_LineEdit->setText("");
+                ui->deadlines_LineEdit->setText("");
+                ui->main_LineEdit->setText("");
+            }
+            if (!checker)
+                QMessageBox::about(this,"Error!", "Что-то полшло не так!"+_prevdirection);
+        }
+    }
+}
