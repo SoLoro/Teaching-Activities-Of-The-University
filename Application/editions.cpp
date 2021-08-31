9d4ae4c0f369886d565c8bd74f2392f3caff3a9a
Viacheslav <ggg9gg6@yandex.ru>
2021-08-31 21:53:57 +0300

    Handling with data to fill in fields using a database with query execution (editions.cpp)

--- Application/editions.cpp
+++ Application/editions.cpp
@@ -18,6 +18,53 @@ editions::editions(QString title, bool isEditing) :
     Editing = isEditing;
     QSqlDatabase myApplicationDB = QSqlDatabase::database("database");
 
+    if (Editing)
+    {
+        QSqlQuery thisQuery = QSqlQuery(myApplicationDB);
+
+        thisQuery.prepare("SELECT edition_name from editions WHERE edition_title = :_newtitle");
+        thisQuery.bindValue(":_newtitle", _prevtitle);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->name_LineEdit->setText(thisQuery.value(0).toString());
+
+
+        thisQuery.prepare("SELECT edition_type from editions WHERE edition_title = :_newtitle");
+        thisQuery.bindValue(":_newtitle", _prevtitle);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->type_LineEdit->setText(thisQuery.value(0).toString());
+
+
+        thisQuery.prepare("SELECT edition_source from editions WHERE edition_title = :_newtitle");
+        thisQuery.bindValue(":_newtitle", _prevtitle);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->source_LineEdit->setText(thisQuery.value(0).toString());
+
+
+        thisQuery.prepare("SELECT edition_pages from editions WHERE edition_title = :_newtitle");
+        thisQuery.bindValue(":_newtitle", _prevtitle);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->pages_LineEdit->setText(thisQuery.value(0).toString());
+
+
+        thisQuery.prepare("SELECT edition_status from editions WHERE edition_title = :_newtitle");
+        thisQuery.bindValue(":_newtitle", _prevtitle);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->status_LineEdit->setText(thisQuery.value(0).toString());
+
+
+        thisQuery.prepare("SELECT edition_contacts from editions WHERE edition_title = :_newtitle");
+        thisQuery.bindValue(":_newtitle", _prevtitle);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->contacts_LineEdit->setText(thisQuery.value(0).toString());
+
+    }
+
 }
 
 editions::~editions()
