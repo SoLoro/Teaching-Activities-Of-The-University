0a5d904c24a59b3c7f8ecc16b0771962e49611fa
Viacheslav <ggg9gg6@yandex.ru>
2021-08-31 22:14:56 +0300

    Initialization teachers

--- Application/teachers.cpp
+++ Application/teachers.cpp
@@ -1,11 +1,18 @@
 #include "teachers.h"
 #include "ui_teachers.h"
 
-teachers::teachers(QWidget *parent) :
-    QWidget(parent),
+#include <QSqlQuery>
+#include <QMessageBox>
+
+teachers::teachers(QString name, bool isEditing) :
     ui(new Ui::teachers)
 {
     ui->setupUi(this);
+    teachers::setWindowTitle("teachers");
+    ui->name_LineEdit->setText(name);
+    _prevname = name;
+    Editing = isEditing;
+    QSqlDatabase myApplicationDB = QSqlDatabase::database("database");
 }
 
 teachers::~teachers()
--- Application/teachers.h
+++ Application/teachers.h
@@ -2,21 +2,36 @@
 #define TEACHERS_H
 
 #include <QWidget>
+#include <QDialog>
+#include <QSql>
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QString>
+#include <QSqlQuery>
+#include <QSystemTrayIcon>
 
 namespace Ui {
 class teachers;
 }
 
-class teachers : public QWidget
+class teachers : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit teachers(QWidget *parent = nullptr);
+    explicit teachers(QString name, bool isEditing);
     ~teachers();
+
+private slots:
+    void on_saveButton_clicked();
 
 private:
     Ui::teachers *ui;
+    QSqlDatabase myApplicationDB;
+    bool Editing;
+    QString _prevname;
+    QSystemTrayIcon* trayIcon;
+
 };
 
 #endif // TEACHERS_H
