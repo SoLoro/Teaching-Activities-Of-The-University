921a318a39ec402ca46531ae9ef0712f9d1688f4
Viacheslav <ggg9gg6@yandex.ru>
2021-08-31 23:10:39 +0300

    Creating a Handler for the result of clicking the pushButton (pulpits.cpp)

--- Application/pulpits.cpp
+++ Application/pulpits.cpp
@@ -42,3 +42,47 @@ pulpits::~pulpits()
 {
     delete ui;
 }
+
+
+void pulpits::on_pushButton_clicked()
+{
+    if (Editing)
+    {
+        QSqlQuery thisQuery = QSqlQuery(myApplicationDB);
+        thisQuery.prepare("SELECT * from function_change_pulpit(:_id ,:_newtitle , :_newtelephone, :_newhead, :_newteachername , :_newworkplandirection , :_neweditiontitle );");
+        thisQuery.bindValue(":_id", realID);
+        thisQuery.bindValue(":_newtitle", ui->titleEdit->text());
+        thisQuery.bindValue(":_newtelephone", ui->telephoneEdit->text());
+        thisQuery.bindValue(":_newhead", ui->headEdit->text());
+        thisQuery.bindValue(":_newteachername", ui->teacherComboBox->currentText());
+        thisQuery.bindValue(":_newworkplandirection", ui->workplanComboBox->currentText());
+        thisQuery.bindValue(":_neweditiontitle", ui->editionCombobox->currentText());
+        bool checker = thisQuery.exec();
+        if (checker)
+        {
+            QMessageBox::about(this,"Great!", "pulpits changed!");
+        }
+        if (!checker){
+            QMessageBox::about(this,"Error!", "Something went wrong!");}
+    }
+
+    if (!Editing)
+    {
+        QSqlQuery thisQuery = QSqlQuery(myApplicationDB);
+        thisQuery.prepare("SELECT * from function_add_pulpit(:_title, :_telephone , :_head , :_teachername, :_workplandirection , :_editiontitle );");
+        thisQuery.bindValue(":_title", ui->titleEdit->text());
+        thisQuery.bindValue(":_telephone", ui->telephoneEdit->text());
+        thisQuery.bindValue(":_head", ui->headEdit->text());
+        thisQuery.bindValue(":_teachername", ui->teacherComboBox->currentText());
+        thisQuery.bindValue(":_workplandirection", ui->workplanComboBox->currentText());
+        thisQuery.bindValue(":_editiontitle", ui->editionCombobox->currentText());
+        bool checker = thisQuery.exec();
+        if (checker)
+        {
+            QMessageBox::about(this,"Great!", "Новые кафедры!");
+        }
+        if (!checker)
+            QMessageBox::about(this,"Error!", "Что-то пошло не так!");
+    }
+}
+
