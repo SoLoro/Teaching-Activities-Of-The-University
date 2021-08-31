be81d344e7906aac000fe102ce57bcdd55797481
Viacheslav <ggg9gg6@yandex.ru>
2021-08-31 21:47:49 +0300

    Initialization editions

--- Application/editions.cpp
+++ Application/editions.cpp
@@ -1,11 +1,23 @@
 #include "editions.h"
 #include "ui_editions.h"
+#include <QSql>
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QString>
+#include <QSystemTrayIcon>
+#include <QSqlQuery>
+#include <QMessageBox>
 
-editions::editions(QWidget *parent) :
-    QWidget(parent),
+editions::editions(QString title, bool isEditing) :
     ui(new Ui::editions)
 {
     ui->setupUi(this);
+    editions::setWindowTitle("Editions");
+    ui->title_LineEdit->setText(title);
+    _prevtitle = title;
+    Editing = isEditing;
+    QSqlDatabase myApplicationDB = QSqlDatabase::database("database");
+
 }
 
 editions::~editions()
