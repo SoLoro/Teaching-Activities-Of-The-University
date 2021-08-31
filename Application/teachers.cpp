6ab6294a546c38607750d21fd2fbf56523f65a1f
Viacheslav <ggg9gg6@yandex.ru>
2021-08-31 22:18:02 +0300

    Handling with data to fill in fields using a database with query execution (teachers.cpp)

--- Application/teachers.cpp
+++ Application/teachers.cpp
@@ -13,6 +13,31 @@ teachers::teachers(QString name, bool isEditing) :
     _prevname = name;
     Editing = isEditing;
     QSqlDatabase myApplicationDB = QSqlDatabase::database("database");
+
+    if (Editing)
+    {
+        QSqlQuery thisQuery = QSqlQuery(myApplicationDB);
+        thisQuery.prepare("SELECT teachers_discipline from teachers WHERE teacher_name = :_newname");
+        thisQuery.bindValue(":_newname", _prevname);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->discipline_LineEdit->setText(thisQuery.value(0).toString());
+
+        thisQuery = QSqlQuery(myApplicationDB);
+        thisQuery.prepare("SELECT teachers_position from teachers WHERE teacher_name = :_newname");
+        thisQuery.bindValue(":_newname", _prevname);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->position_LineEdit->setText(thisQuery.value(0).toString());
+
+
+        thisQuery = QSqlQuery(myApplicationDB);
+        thisQuery.prepare("SELECT teachers_telephone from teachers WHERE teacher_name = :_newname");
+        thisQuery.bindValue(":_newname", _prevname);
+        thisQuery.exec();
+        thisQuery.first();
+        ui->telephone_LineEdit->setText(thisQuery.value(0).toString());
+    }
 }
 
 teachers::~teachers()
