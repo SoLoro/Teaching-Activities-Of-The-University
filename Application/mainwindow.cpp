1fd9cecd4a304c8aa0ee4a8bfd751a2fc812f76f
Viacheslav <ggg9gg6@yandex.ru>
2021-08-31 21:30:34 +0300

    Development of the authorization window functionality (mainwindow)

--- Application/mainwindow.cpp
+++ Application/mainwindow.cpp
@@ -6,10 +6,36 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->Password_lineEdit->setEchoMode(QLineEdit::Password);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    login = "";
+    password = "";
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    login = ui->Login_lineEdit->text();
+    password = ui->Password_lineEdit->text();
+    myApplicationDB = QSqlDatabase::addDatabase("QPSQL");
+    myApplicationDB.setHostName("localhost");
+    myApplicationDB.setDatabaseName("database");
+    myApplicationDB.setUserName(login);
+    myApplicationDB.setPassword(password);
+    bool checker = myApplicationDB.open();
+    if (checker)
+    {
+        myApplication = new application();
+        myApplication->show();
+        this->close();
+    }
+    else
+        QMessageBox::about(this, "Error", "Возникла ошибка входа (Неверное сочетание логин + пароль)");
+    ui->Login_lineEdit->setText("");
+    ui->Password_lineEdit->setText("");
 }
 
--- Application/mainwindow.h
+++ Application/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QSql>
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QDebug>
+#include <QMessageBox>
+#include <application.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +22,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+
 private:
     Ui::MainWindow *ui;
+    application* myApplication;
+    QSqlDatabase myApplicationDB;
+    QString login;
+    QString password;
 };
 #endif // MAINWINDOW_H
